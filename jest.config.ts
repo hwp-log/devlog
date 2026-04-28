14bf0f7e129cffc0041501b515fc3895166a7cb1
hwp2024dev <hwp2024dev@gmail.com>
2026-04-28 22:52:32 +0900

    chore: Jest 설정 파일 추가

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+// Jest 실행 환경 설정 (어떻게 테스트할지)
+import nextJest from 'next/jest.js'
+
+const createJestConfig = nextJest({
+    dir: './',
+})
+
+const config = {
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/$1',
+    },
+}
+
+export default createJestConfig(config)
---
+++ jest.setup.ts
@@ -0,0 +1,2 @@
+// 테스트 공통 준비
+import '@testing-library/jest-dom'
